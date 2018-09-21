ede0813fad76978c372eacc9001819a243ffe7a6
Justin Lee <justin.lee22@Y22-Design-Justin-Lee.local>
2018-09-21 17:03:57 -0400

    Addition

---
+++ Annoying.py
@@ -0,0 +1,17 @@
+import os
+
+
+while(True) :
+	os.system("say hi")
+
+
+	answer = input("Type exit to stop: ")
+	os.system("say hi")
+	
+	if answer == 'exit':
+		break
+	else:
+		os.system("say hi")
+
+
+	
