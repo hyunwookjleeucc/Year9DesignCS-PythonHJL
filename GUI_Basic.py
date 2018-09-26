b5b7bb34ce8bbcdda12f97f79bacd584287e644b
Justin Lee <justin.lee22@Y22-Design-Justin-Lee.local>
2018-09-25 23:49:13 -0400

    GUI_Basic_VolofCyl.py

---
+++ GUI_Basic.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+import math
+
+
+def calculate():
+
+	r = float(entr.get())
+	h = float(enth.get())
+
+	v = math.pi*r*r*h
+	v = round(v,3)
+
+	output.config(state="normal")
+
+	outputValue= "Given\nradius:" +str(r) + " units\nheight:" +str(h) + " units\nThe volume is:" + str(v) + " units cubed"
+
+
+	output.delete(1.0, tk.END)
+	output.insert (tk.INSERT, outputValue)
+	output.config(state="disabled")
+
+
+root = tk.Tk()
+root.title("Volume of a cylinder")
+
+
+labr = tk.Label(root, text="radius")
+labr.pack()
+
+entr = tk.Entry(root)
+entr.pack()
+
+labh = tk.Label(root, text = "height")
+labh.pack()
+
+enth = tk.Entry(root)
+enth.pack()
+
+btn = tk.Button(root, text="Calculate", command=calculate)
+btn.pack()
+
+output = tk.Text(root, width=50, height=10, borderwidth=3, relief=tk.GROOVE)
+output.config(state="disabled")
+output.pack()
+
+
+
+
+
+
+
+
+root.mainloop()
